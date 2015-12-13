e821fcf56461a4a84e98331407a8273d5ed6084a
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-12 18:24:22 -0600

    mv make-fn, make-polymer-fns to miraj.markdown

--- src/main/clojure/miraj/markup.clj
+++ src/main/clojure/miraj/markup.clj
@@ -28,6 +28,106 @@
 (defonce mode (atom nil))
 (defonce miraj-boolean-tag "__MIRAJ_BOOLEAN_955196")
 
+(defn make-fns
+  [args]
+  ;; (log/trace "make-fns " args) ;; (type args))
+  (doseq [arg args]
+    ;; (log/trace "make-fns arg: " arg (type arg))
+    (let [farg (symbol arg)
+          kw   (keyword arg)
+          func `(defn ~farg ;; (symbol (str arg))
+                  [& hargs#]
+                  ;; (log/trace "HTML FN: " ~kw (pr-str hargs#))
+                  (if (empty? hargs#)
+                    (xml/element ~kw)
+                    (let [first# (first hargs#)
+                          attrs# (if (map? first#)
+                                   (do ;(log/trace "map? first")
+                                       (if (instance? miraj.markup.Element first#)
+                                         (do ;(log/trace "Element instance")
+                                             {})
+                                         (do ;(log/trace "NOT Element instance")
+                                             first#)))
+                                   (do ;(log/trace "NOT map? first")
+                                       {}))
+                          content# (if (map? first#)
+                                     (if (instance? miraj.markup.Element first#)
+                                       hargs#
+                                       (rest hargs#))
+                                     hargs#)
+                          func# (apply xml/element ~kw attrs# content#)]
+                      ;; (log/trace "hargs: " hargs#)
+                      ;; (log/trace "kw: " ~kw)
+                      ;; (log/trace "args: " attrs#)
+                      ;; (log/trace "content: " content# " (" (type content#) ")")
+                      ;; (log/trace "func: " func# (type func#))
+                      func#)))
+          f (eval func)])))
+
+(defn make-void-elt-fns
+  [args]
+  ;; (log/trace "make-void-elt-fns " args) ;; (type args))
+  (doseq [arg args]
+    ;; (log/trace "make-void-elt-fns fn: " arg) ;; (type arg))
+    (let [farg (symbol arg)
+          kw   (keyword arg)
+          func `(defn ~farg ;; (symbol (str arg))
+                  [& hargs#]
+                  ;; (log/trace "HTML VOID FN: " ~kw (pr-str hargs#))
+                  (if (empty? hargs#)
+                    (xml/element ~kw)
+                    (if (not (map? (first hargs#)))
+                      (throw (Exception. (str "content not allowed in HTML void element " ~kw)))
+                      (if (instance? miraj.markup.Element (first hargs#))
+                        (throw (Exception. (str "content not allowed in HTML void element " ~kw)))
+                        (if (not (empty? (rest hargs#)))
+                          (throw (Exception. (str "content not allowed in HTML void element " ~kw)))
+                        (let [func# (apply xml/element ~kw hargs#)]
+                        ;;   ;; (log/trace "hargs: " hargs#)
+                          ;; (log/trace "kw: " ~kw)
+                          ;; (log/trace "args: " (first hargs#))
+                          ;; (log/trace "func: " func# (type func#))
+                          func#))))))
+          f (eval func)])))
+
+(defn make-polymer-fns
+  [pfx args]
+  (log/trace "make-polymer-fns " pfx) ;; " " args) ;; (type args))
+  (doseq [arg args]
+    (let [farg (symbol arg)
+          kw (keyword (str pfx "-" arg))
+          ;; log (println "make-polymer-fns arg: " farg " (" arg ")")
+          func `(defn ~farg ;; (symbol (str arg))
+                  [& hargs#]
+                  ;; (println "POLYMER FN: " ~kw (pr-str hargs#))
+                  (if (empty? hargs#)
+                    (xml/element ~kw)
+                    (let [first# (first hargs#)
+                          attrs# (if (map? first#)
+                                   (do ;(log/trace "map? first")
+                                       (if (instance? miraj.markup.Element first#)
+                                         (do ;(log/trace "Element instance")
+                                             {})
+                                         (do ;(log/trace "NOT Element instance")
+                                             first#)))
+                                   (do ;(log/trace "NOT map? first")
+                                       {}))
+                          content# (if (map? first#)
+                                     (if (instance? miraj.markup.Element first#)
+                                       hargs#
+                                       (rest hargs#))
+                                     hargs#)
+                          func# (apply xml/element ~kw attrs# content#)]
+                      ;; (log/trace "hargs: " hargs#)
+                      ;; (log/trace "kw: " ~kw)
+                      ;; (log/trace "args: " attrs#)
+                      ;; (log/trace "content: " content# " (" (type content#) ")")
+                      ;; (log/trace "func: " func# (type func#))
+                      func#)))
+          f (eval func)])))
+
+
+
 ; Represents a parse event.
 ; type is one of :start-element, :end-element, or :characters
 (defrecord Event [type name attrs str])
