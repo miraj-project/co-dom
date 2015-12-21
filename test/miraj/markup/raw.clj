9d077f8bed224e62d09ac0a1e6320b20d4237477
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-21 05:06:17 -0600

    remove unrelated test files

--- test/miraj/markup/raw.clj
+++
@@ -1,7 +0,0 @@
-(ns raw
-  (:require [miraj.markup :as xml]
-            [clojure.tools.logging :as log :only [trace debug error info]]
-            [clojure.pprint :as pp]
-            [clojure.string :as string]
-            [clojure.test]))
-
