e32d87e6dcf2d06ffc87aa0dd1ed68c9958a3172
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-22 08:13:09 -0600

    fix proj deps

--- project.clj
+++ project.clj
@@ -2,17 +2,16 @@
   :description "HTML5/Polymer in Clojure"
   :url "https://github.com/mobileink/miraj.markup"
   :source-paths ["src/main/clojure"]
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/data.xml "0.0.8"]
-                 [org.slf4j/slf4j-log4j12 "1.7.1"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jmdk/jmxtools
-                                                    com.sun.jmx/jmxri]]
-                 [ clj-logging-config "1.9.7"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]]
   :profiles {:dev {:prep-tasks ^:replace ["clean" "compile"]
-                   :source-paths ["src/main/clojure" "src/test/clojure" "dev"]}
+                   :source-paths ["src/main/clojure" "src/test/clojure" "dev"]
+                   :dependencies [[org.clojure/tools.logging "0.3.1"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.1"]
+                                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                                     javax.jms/jms
+                                                                     com.sun.jmdk/jmxtools
+                                                                     com.sun.jmx/jmxri]]
+                                  [ clj-logging-config "1.9.7"]]}
              :test {:resource-paths ["test/resources"]
                     :dependencies [[miraj "1.1.4-SNAPSHOT"]
                                    [polymer/paper "1.2.3-SNAPSHOT"]
