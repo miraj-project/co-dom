a8c160e2cb53f73a0eb689a9c685ac23686657a1
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-25 11:31:18 -0600

    add deps for dev profile

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
   :dependencies [[org.clojure/clojure "1.8.0-RC4"]]
   :profiles {:dev {:prep-tasks ^:replace ["clean" "compile"]
                    :source-paths ["src/main/clojure" "src/test/clojure" "dev"]
-                   :dependencies [[org.clojure/tools.logging "0.3.1"]
+                   :dependencies [[polymer/paper "1.2.3-SNAPSHOT"]
+                                  [polymer/iron "1.2.3-SNAPSHOT"]
+                                  [miraj/html "5.1.0-SNAPSHOT"]
+                                  [org.clojure/tools.namespace "0.2.1"]
+                                  [org.clojure/tools.logging "0.3.1"]
                                   [org.slf4j/slf4j-log4j12 "1.7.1"]
                                   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                                      javax.jms/jms
