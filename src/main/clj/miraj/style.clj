e4731f91b63b9e614207a9996565b9ebe0e4135b
Gregg Reynolds <dev@mobileink.com>
2017-05-10 08:54:54 -0500

    add miraj.style ns
    Signed-off-by: Gregg Reynolds <dev@mobileink.com>

---
+++ src/main/clj/miraj/style.clj
@@ -0,0 +1,15 @@
+(ns miraj.style)
+
+;; placeholder, for style attrib namespacing
+
+(def pseudo-attrs
+  #{::active ::checked ::disabled ::enabled ::focus ::hover ::indeterminate ::lang ::link ::target ::visited
+    ::root ::nth-child ::nth-last-child ::nth-of-type ::nth-last-of-type ::first-child ::last-child ::first-of-type ::last-of-type ::only-child ::only-of-type ::empty
+    ::after ::before ::first-line ::first-letter})
+
+
+;; (def pseudo-attrs
+;;   #{:$active :$checked :$disabled :$enabled :$focus :$hover :$indeterminate :$lang :$link :$target :$visited
+;;     :$root :$nth-child :$nth-last-child :$nth-of-type :$nth-last-of-type :$first-child :$last-child :$first-of-type :$last-of-type :$only-child :$only-of-type :$empty
+;;     :$after :$before :$first-line :$first-letter})
+
